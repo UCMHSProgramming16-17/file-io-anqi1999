b451e6ab66c7b750278f90f54ee7985239209626
Anqi Xu <axu@ucvts.org>
2017-01-02 17:17:45 +0000

    fixed the order in csv

--- life-expectancy.py
+++ life-expectancy.py
@@ -3,25 +3,6 @@ import requests
 import csv
 import datetime
 
-# url
-endpoint = 'http://api.population.io:80/1.0/life-expectancy/remaining/'
-
-# parameters
-# sex = input("Are you male or female? (Apologies for the societal gender norms. The API doesn't take into account the whole spectrum, I'm sorry.) ")
-# country = input("Which country would you call your home? Or if you'd like to see how fast you'd die in a different country, be my guest. ")
-# date = input("Today's date? If you can remember. Who keeps track of the date these days, anyway? YYYY-MM-DD. ")
-# years = input("Your age in years? Could be real or made up. Depends on if you actually care about your estimated time of death. ")
-# months = input("And how many months into that age are you? ")
-# days = input("And how many days into that month, if you're willing to calculat it? If not, just enter '0' and be done with it. ")
-
-# assemble
-# url = endpoint + sex + '/' + country + '/' + date + '/' + years + 'y' + months + 'm' + days + 'd'
-# r = requests.get(url)
-
-# life expectancy
-# stats = r.json()
-# remaining = stats['remaining_life_expectancy']
-
 # PARAMETERS:
 # sex
 sex = ['male', 'female']
@@ -35,10 +16,10 @@ country = list_countries['countries']
 # age
 age = []
 for num in range(81):
-    age.append(num + 'y')
+    age.append(str(num) + 'y')
 
 # date
-date = datetime.date.today()
+date = str(datetime.date.today())
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -50,6 +31,18 @@ lifechart = open('lifechart.csv', 'w', newline='')
 w = csv.writer(lifechart, delimiter=',')
 
 # write to the file
-w.writerow(['sex', 'age', 'country', 'life expectancy'])
-for gender in sex:
-    
+w.writerow(['country', 'age', 'sex', 'life remaining, in years'])
+for c in country:
+    for a in age:
+        for s in sex:
+            # get the remaining life expectancy
+            endpoint = 'http://api.population.io:80/1.0/life-expectancy/remaining/'
+            url = endpoint + s + '/' + c + '/' + date + '/' + a
+            r = requests.get(url)
+            stats = r.json()
+            remaining = stats['remaining_life_expectancy']
+            # write each to the file
+            w.writerow([c, a, s, remaining])
+            
+# close file
+lifechart.close()
