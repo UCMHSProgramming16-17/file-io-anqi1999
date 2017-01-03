191d08fd6bb320290c1703357570ed827b82cb69
Anqi Xu <axu@ucvts.org>
2017-01-03 17:55:33 +0000

    fixed problems!

--- life-expectancy.py
+++ life-expectancy.py
@@ -2,13 +2,21 @@
 import requests
 import csv
 import datetime
+import math
 
 # PARAMETERS:
 # sex
 sex = ['male', 'female']
 
 # country
-country = input('Country? ')
+c_url = 'http://api.population.io:80/1.0/countries'     # get the available list of countries
+c = requests.get(c_url)
+c_list = c.json()
+c_available = c_list['countries']
+
+country = input('Country? Preferably its official name, and in proper English. ')      # get user's input
+if country not in c_available:
+    country = input('Sorry, your country is not in the list of available countries. :/ Either you misspelled, did not capitalize properly, forgot the official name of your country, or you live in a smaller country. Try again. ')
 
 # age
 age = []
@@ -20,6 +28,41 @@ date = str(datetime.date.today())
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~
 
+# FUNCTION TO CONVERT FROM YEARS DECIMAL TO 'YEARS, MONTHS, DAYS':
+# set empty variable
+life_expectancy = 'blah'
+
+# define function
+def years(remaining):
+    global life_expectancy
+    
+    rem = float(remaining)
+    year = math.floor(rem)        # number of full years
+    m = (rem - year) * 12
+    month = math.floor(m)       # number of full months
+    d = (m - month) * 30
+    day = math.floor(d)         # number of full days
+    
+    life_expectancy = str(year) + ' years, ' + str(month) + ' months, ' + str(day) + ' days'
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# FUNCTION FOR THE REMAINING LIFE API:
+# set empty variable
+remaining = 'blah'
+
+# define function
+def api():
+    global remaining
+    
+    endpoint = 'http://api.population.io:80/1.0/life-expectancy/remaining/'     # remaining lifetime api
+    url = endpoint + s + '/' + country + '/' + date + '/' + a
+    r = requests.get(url)
+    stats = r.json()
+    remaining = stats['remaining_life_expectancy']
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~
+
 # CSV:
 # new csv file
 lifechart = open('lifechart.csv', 'w', newline='')
@@ -28,17 +71,14 @@ lifechart = open('lifechart.csv', 'w', newline='')
 w = csv.writer(lifechart, delimiter=',')
 
 # write to the file
-w.writerow(['age', 'sex', 'life remaining, in years'])
+w.writerow(['age', 'sex', 'life remaining'])
 for a in age:
     for s in sex:
-        # get the remaining life expectancy
-        endpoint = 'http://api.population.io:80/1.0/life-expectancy/remaining/'
-        url = endpoint + s + '/' + country + '/' + date + '/' + a
-        r = requests.get(url)
-        stats = r.json()
-        remaining = stats['remaining_life_expectancy']
-        # write each to the file
-        w.writerow([a, s, remaining])
+        api() # call api function for remaining life expectancy
+        
+        years(remaining)        # call years converter function
+        
+        w.writerow([a, s, life_expectancy])     # write 'age', 'sex', and 'remaining life expectancy' to the file
             
 # close file
 lifechart.close()
